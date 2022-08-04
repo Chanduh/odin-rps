ba32b0b8ba31c40bd8391cc5ddd211424ec4864b
Chanda Gonet <chandagonet@gmail.com>
2022-08-04 12:22:11 -0400

    Make functional

--- script.js
+++ script.js
@@ -1,72 +1,88 @@
+function compVoice(string) {
+    document.getElementById('computerVoice').innerHTML
+        = string;
+}
+function tallyHuman() {
+    document.getElementById('scoreboardHuman').innerHTML
+        += "|";
+        humanScore++;
+}
+function tallyComputer() {
+    document.getElementById('scoreboardComputer').innerHTML
+        += "|";
+        computerScore++;
 
-let computerVoice = "Let's play a game..."
-let computerSelection = getComputerChoice();
+}
 
+
+let playerSelection;
+let humanScore = 0;
+let computerScore = 0;
+
+
+// INITIATES GAME PLAY / GETS PLAYER INPUT
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
-      rock.addEventListener("click", function(rock){
-        playerSelection = "ROCK";
-        let result = playRound("ROCK", computerSelection);
-        return result;
-      });
-
-      
-      paper.addEventListener("click", function(paper){
-        playerSelection = "PAPER"
-      });
-      scissors.addEventListener("click", function(scissors){
-        playerSelection = "SCISSORS"
-      });
+rock.addEventListener("click", function(){
+    playRound("ROCK", getComputerChoice());
+});
+paper.addEventListener("click", function(){
+    playRound("PAPER", getComputerChoice());
+});
+scissors.addEventListener("click", function(){
+    playRound("SCISSORS", getComputerChoice());
+});
               
-
-
-
+// RANDOM COMPUTER INPUT
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0){return "ROCK"}
     else if (randomNumber === 1){return "PAPER"}
     else if (randomNumber === 2){return "SCISSORS"}
 }
+function checkForGameOver(){
+if (computerScore >= 5 || humanScore >= 5){
+    location.reload();
 
-let scoreboardHuman = "0";
-let scoreboardComputer = "0";
+}
+}
+function checkForWinner(){
+    if (computerScore >= 5){
+        computerVoice.style.color = "red";
+        scoreboardHuman.style.color = "black";
+        scoreboardComputer.style.color = "red";
+        compVoice("That was so easy for me, you know.")
+    }
+    else if (humanScore >= 5){
+        computerVoice.style.color = "gray";
+        scoreboardComputer.style.color = "black";
+        scoreboardHuman.style.color = "red";
+        compVoice("Whatever, I let you win.")
+    }
+    else {}
+}
 
-function playRound(playerSelection, computerSelection){
+// GAMEPLAY
+function playRound(playerSelection, func){
 
-
-    return function(){
-        if (scoreboardHuman > 3 || scoreboardComputer > 3){
-            computerVoice = `We have a winner! You have ${scoreboardHuman}, I have ${scoreboardComputer};
-            return;`
-        }
-        else if (playerSelection === computerSelection){
-            computerVoice = "Tie!"
+         checkForGameOver();
+         computerSelection = getComputerChoice()
+    
+        if (playerSelection === computerSelection){
+            compVoice("Eh, it's a tie!");
         }
         else if ((computerSelection == "ROCK" && playerSelection == "PAPER")||
             (computerSelection == "PAPER" && playerSelection == "SCISSORS")||
             (computerSelection == "SCISSORS" && playerSelection == "ROCK")){
-                scoreboardHuman++;
-                computerVoice = "Damn, you!";
+            compVoice("Damn, you!");
+            tallyHuman();
         }
-    }
+        else {
+        tallyComputer();
+        compVoice("Obviously.")     
+        } 
 
+        checkForWinner();
 }
-
-
- 
- 
-
-    document.getElementById('computerVoice').innerHTML = computerVoice
-    document.getElementById('scoreboardHuman').innerHTML = scoreboardHuman
-    document.getElementById('scoreboardComputer').innerHTML = scoreboardComputer
-
-
-
-
-
-
-
-
-
